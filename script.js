ab77221f42a1c77d2b80038315a34a0b10815776
ccarlaa <carlacarlaclementino@gmail.com>
2022-02-10 17:51:08 -0300

    Carregar mensagens do servidor

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function fecharMenu(){
     let menufechado = document.getElementById("menu").style.display="none";
     let opaco = document.getElementById("opaco").style.display="none";
     document.documentElement.style.overflow = '';
-
 }
 
 function abrirMenu() {
@@ -23,7 +22,7 @@ while(nome == ""){
     nome = prompt("Digite seu nome para entrar no chat:");
 }
 
-promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
 promessa.then(enviarResposta);
 promessa.catch(deuRuim);
 
@@ -33,8 +32,9 @@ function enviarResposta(resposta) {
     for(let i = 0 ; i < informacoes.length ; i++){
         console.log(i)
         let mensagens = document.querySelector(".chat")
-        mensagens.innerHTML += `<div class="tentativa"><p> ${informacoes[i].name} </p><p> ${informacoes[i].from} </p><p> ${informacoes[i].text} </p></div>`
+        mensagens.innerHTML += `<div class="mensagensrecebidasstatus"><div class="tamanhodamsg"><p class="tempo">(${informacoes[i].time})</p><p class="para"><b>${informacoes[i].from}</b> to <b>${informacoes[i].to}:</b> </p><p class="texto"> ${informacoes[i].text}</p></div></div><div class="espaco"></div>`
     }
+
 }
 
 function deuRuim(){
