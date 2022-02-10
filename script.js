b4aebfd63453e4c405e1721d146287654854b002
ccarlaa <carlacarlaclementino@gmail.com>
2022-02-10 17:53:26 -0300

    Recarregar mensagens do servidor após 3s

--- script.js
+++ script.js
@@ -34,7 +34,15 @@ function enviarResposta(resposta) {
         let mensagens = document.querySelector(".chat")
         mensagens.innerHTML += `<div class="mensagensrecebidasstatus"><div class="tamanhodamsg"><p class="tempo">(${informacoes[i].time})</p><p class="para"><b>${informacoes[i].from}</b> to <b>${informacoes[i].to}:</b> </p><p class="texto"> ${informacoes[i].text}</p></div></div><div class="espaco"></div>`
     }
+    recarregarChat()
+}
 
+function recarregarChat(){ //Se as cartas são diferentes 
+    setTimeout(() => {
+        promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+        promessa.then(enviarResposta);
+    },3000);
+    console.log(enviarResposta)
 }
 
 function deuRuim(){
