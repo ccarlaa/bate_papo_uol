101fe243dff54fb076ab0e7385771a67ba8a8991
ccarlaa <carlacarlaclementino@gmail.com>
2022-02-10 09:41:22 -0300

    Menu lateral com fundo opaco

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// -----BONUS -> FECHAR MENU-------
+
+
+function fecharMenu(){
+    let menufechado = document.getElementById("menu").style.display="none"
+    let opaco = document.getElementById("opaco").style.display="none"
+
+}
+
+function abrirMenu() {
+    let menuaberto = document.getElementById("menu").style.display="flex"
+    let opaco = document.getElementById("opaco").style.display="flex"
+
+}
