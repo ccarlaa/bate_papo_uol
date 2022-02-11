b9b4fce31248ed9d1bf1088e373d83a0882957dc
ccarlaa <carlacarlaclementino@gmail.com>
2022-02-10 21:23:51 -0300

    Mudar a cor das mensagens de acordo com seu status

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function abrirMenu() {
 
 let nome = prompt("Digite seu nome para entrar no chat:");
 let promessa;
+let mensagens
 
 while(nome == ""){
     nome = prompt("Digite seu nome para entrar no chat:");
@@ -30,19 +31,32 @@ function enviarResposta(resposta) {
     let informacoes = resposta.data;
     console.log(informacoes);
     for(let i = 0 ; i < informacoes.length ; i++){
+        if(informacoes[i].type == "status"){
         console.log(i)
-        let mensagens = document.querySelector(".chat")
+        mensagens = document.querySelector(".chat")
         mensagens.innerHTML += `<div class="mensagensrecebidasstatus"><div class="tamanhodamsg"><p class="tempo">(${informacoes[i].time})</p><p class="para"><b>${informacoes[i].from}</b> to <b>${informacoes[i].to}:</b> </p><p class="texto"> ${informacoes[i].text}</p></div></div><div class="espaco"></div>`
+        }else if(informacoes[i].type == "message"){
+        console.log(i)
+        mensagens = document.querySelector(".chat")
+        mensagens.innerHTML += `<div class="mensagensrecebidasmessage"><div class="tamanhodamsg"><p class="tempo">(${informacoes[i].time})</p><p class="para"><b>${informacoes[i].from}</b> to <b>${informacoes[i].to}:</b> </p><p class="texto"> ${informacoes[i].text}</p></div></div><div class="espaco"></div>`
+        }else{
+        console.log(i)
+        mensagens = document.querySelector(".chat")
+        mensagens.innerHTML += `<div class="mensagensrecebidasreservada"><div class="tamanhodamsg"><p class="tempo">(${informacoes[i].time})</p><p class="para"><b>${informacoes[i].from}</b> to <b>${informacoes[i].to}:</b> </p><p class="texto"> ${informacoes[i].text}</p></div></div><div class="espaco"></div>`
+        }
     }
     recarregarChat()
+
 }
 
+
 function recarregarChat(){ //Se as cartas são diferentes 
-    setTimeout(() => {
-        promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
-        promessa.then(enviarResposta);
-    },3000);
-    console.log(enviarResposta)
+    // setTimeout(() => {
+    //     mensagens.innerHTML="";
+    //     promessa = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
+    //     promessa.then(enviarResposta);
+    // },3000);
+    // console.log(enviarResposta)
 }
 
 function deuRuim(){
